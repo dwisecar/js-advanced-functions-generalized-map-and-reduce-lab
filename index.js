ddff84aec0725cab02ed856cda9685189a9ce2a1
dwisecar <dwisecar@gmail.com>
2021-01-19 20:32:26 -0800

    commit tree e449519f79add4fd40aed4f1dd2a405aae576d17

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+ function map(arr, func) {
+    return arr.map(i => func(i))      
+ }
+
+ function reduce(arr, func, start) {
+    let a = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      a = func(arr[i], a)
+    }
+  
+    return a;
+ }
